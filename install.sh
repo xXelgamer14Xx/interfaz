cd3c7d2deb2c9d6f126fcc96ab1eee3e13fc2e96
elgamer14 <143922749+xXelgamer14Xx@users.noreply.github.com>
2023-10-15 00:58:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,12 @@ echo "esto tardardara algo agara una taza de cafe y mientras tanto espera.......
 
 sleep 3
 
+sudo apt install libcurl4=7.74.0-1.3+deb11u10
+sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev
 sudo apt-get install build-essential libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev php-cgi cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libcurl4-openssl-dev libnl-genl-3-dev
+sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev
+
 
 echo " "
 echo "........................tarea finalizada............................."
@@ -191,12 +196,21 @@ echo ""
 echo "......................instalamos y configuramos la polibar :)......................."
 echo ""
 
+sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl3-dev                
+sudo apt update
+sudo apt install libnl-genl3-dev
 sudo apt-get install libuv1-dev
 
-cd  ~/$desk/interfaz/polybar/build
-cmake ~/Descargas/polybar/CMakeLists.txt
+cd ~/Descargas/
+git clone --recursive https://github.com/polybar/polybar
+cd polybar/
+mkdir build
+cd build/
+cmake ..
 make -j$(nproc)
 sudo make install
+ 
+mkdir ~/.config/polybar
 
 echo ".......................este paso puede traer errores asi que te recomiendo que cheques si no tiene errores gracias............................"
 echo ""
@@ -210,6 +224,7 @@ echo ""
 
 sudo apt update && sudo apt upgrade
 sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
+
 
 cd  ~/Descargas
 git clone https://github.com/ibhagwan/picom.git
@@ -226,6 +241,12 @@ sudo ninja -C build install
 
 sudo echo 'picom --experimental-backends &' >> ~/.config/bspwm/bspwmrc
 sudo echo 'bspc config border_width 0' >> ~/.config/bspwm/bspwmrc
+
+cd ~/Descargas/
+git clone https://github.com/VaughnValle/blue-sky.git
+cd ~/Descargas/blue-sky/polybar/
+cp * -r ~/.config/polybar
+
 
 echo ""
 echo ".............configuracion terminada........................."
